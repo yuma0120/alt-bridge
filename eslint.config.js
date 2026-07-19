1a514eda866086b746444765cf4e04af93ce49ef
yuma0120 <yuuma.qawsed@gmail.com>
2026-07-20 01:46:03 +0900

    ci: add release workflow and harden CI pipeline

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,6 @@
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    ignores: ['dist', 'node_modules', 'public']
-  }
-);
+export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, {
+  ignores: ["dist", "node_modules", "public"],
+});
